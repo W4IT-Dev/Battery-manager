e96ad81b0881b211fc5ca40d63fee663aad6dfde
W4IT <w4it.dev.business@gmail.com>
2023-09-02 17:23:03 +0200

    added the icon back (removed it by accident) added temperature to info added back alarm (removed it by accident as well) styling

--- script.js
+++ script.js
@@ -7,51 +7,53 @@ navigator.getBattery().then(function (battery) {
         updateLevelInfo();
         updateChargingInfo();
         updateDischargingInfo();
+        document.querySelector('#temperature').innerText = battery.temperature;
+
     }
     updateAllBatteryInfo();
 
     battery.addEventListener('chargingchange', function () {
         updateChargeInfo();
     });
-    function updateChargeInfo() {
-        a = document.querySelector('#container');
-        battery.charging ? a.classList.add('charging') : a.classList.remove('charging');
-    }
 
     battery.addEventListener('levelchange', function () {
         updateLevelInfo();
     });
-    function updateLevelInfo() {
-        document.querySelector('#level #progress').style.width = battery.level * 100 + "px"
-        document.querySelector('#level p').innerText = battery.level * 100 + "%"
-    }
 
     battery.addEventListener('chargingtimechange', function () {
         updateChargingInfo();
     });
-    function updateChargingInfo() {
-        if (battery.dischargingTime != Infinity) {
-            document.querySelector('#charge').parentNode.style.display = "flex"
-            document.querySelector('#charge').innerText = battery.dischargingTime / 60 + " min"
-            console.log(battery.dischargingTime / 60 + " min")
-            document.querySelector('#charge').parentNode.classList.add('nav')
-        } else document.querySelector('#charge').parentNode.style.display = "none", document.querySelector('#charge').parentNode.classList.remove('nav')
-    }
 
     battery.addEventListener('dischargingtimechange', function () {
         updateDischargingInfo();
     });
+
+    console.log(document.querySelector('#max-charge').value, document.querySelector('#min-level').value)
+    function updateChargeInfo() {
+        a = document.querySelector('#container');
+        battery.charging ? a.classList.add('charging') : a.classList.remove('charging');
+    }
+    function updateLevelInfo() {
+        if (battery.level >= document.querySelector('#max-charge').value || battery.level <= document.querySelector('#min-level').value) alarm.play();
+        document.querySelector('#level #progress').style.width = battery.level * 100 + "px";
+        document.querySelector('#level p').innerText = parseInt(battery.level * 100) + "%";
+    }
+    function updateChargingInfo() {
+        if (battery.chargingTime != Infinity) {
+            document.querySelector('#charge').parentNode.style.display = "flex"
+            document.querySelector('#charge').innerText = parseInt(battery.chargingTime / 60) + " min"
+            document.querySelector('#charge').parentNode.classList.add('nav')
+        } else document.querySelector('#charge').parentNode.style.display = "none", document.querySelector('#charge').parentNode.classList.remove('nav')
+    }
     function updateDischargingInfo() {
         if (battery.dischargingTime != Infinity) {
             document.querySelector('#discharge').parentNode.style.display = "flex"
-            document.querySelector('#discharge').innerText = battery.dischargingTime / 60 + " min"
-            console.log(battery.dischargingTime / 60 + " min")
+            document.querySelector('#discharge').innerText = parseInt(battery.dischargingTime / 60) + " min"
             document.querySelector('#discharge').parentNode.classList.add('nav')
         } else document.querySelector('#discharge').parentNode.style.display = "none", document.querySelector('#discharge').parentNode.classList.remove('nav')
     }
 
 });
-// let wakelock = navigator.requestWakeLock('screen');
 document.addEventListener('keydown', () => {
     if (alarming) alarm.pause(), alarming = false;
 })
@@ -68,7 +70,6 @@ getKaiAd({
 
 
 function nav(move) {
-
     let currentIndex = document.activeElement;
     const items = document.querySelectorAll('.nav')
 
